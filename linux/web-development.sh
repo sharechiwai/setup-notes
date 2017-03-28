e6da2fcd1fdfb4c19ca49a4d049a4b2a47fe6623
sharechiwai <yau.chiwai@gmail.com>
2017-03-28 22:20:09 +0800

    add

--- linux/web-development.sh
+++ linux/web-development.sh
@@ -3,3 +3,4 @@ sudo apt-get install git -y
 echo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash 
 sudo apt-get install -y nodejs
 sudo apt-get install -y ruby-full
+sudo apt-get install -y rubygems
