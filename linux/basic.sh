02bcc428b428dc2929928737714b4de1e53641ac
sharechiwai <yau.chiwai@gmail.com>
2017-06-28 23:03:58 +0800

    added firewall sample

--- linux/basic.sh
+++ linux/basic.sh
@@ -1,2 +1,9 @@
 sudo apt-get update
+# install git
 sudo apt-get install git
+
+# enable firewall for common port
+# enable firewall for OpenSSH
+sudo ufw allow OpenSSH
+# enable firewall
+sudo ufw enable
