61e65958a125735a1be14ce0278eee471e43e631
Chi Wai Yau <chiwai.yau@gravitysupplychain.com>
2018-02-11 09:58:55 +0800

    update config to install curl

--- linux/web-development.sh
+++ linux/web-development.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get install git -y
-echo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y ruby-full
 sudo apt-get install -y rubygems
+sudo apt-get install curl -y
