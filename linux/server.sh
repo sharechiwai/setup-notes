8b7d8dea209156f9aebfd9cb397de382bc9c96b0
Chi Wai Yau <chiwai.yau@gravitysupplychain.com>
2018-03-08 21:03:07 +0800

    added letsencrypt install and renew script

--- linux/server.sh
+++ linux/server.sh
@@ -2,4 +2,9 @@
 sudo apt-get update 
 sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx HTTPS'
 
+# letsencrypt
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update
+sudo apt-get install python-certbot-nginx -y
